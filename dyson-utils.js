d9cc89883f87994a38e946e3acb3a5c70497b1ed
Jean Pierre Wenzel <jeanpierre.wenzel@gmail.com>
2020-10-30 23:12:07 +0100

    Fixed typo in function description

--- dyson-utils.js
+++ dyson-utils.js
@@ -14,7 +14,7 @@ const _ = require('lodash');
 * @param number {number} Value thats needs to be filled up with leading zeros
 * @param width  {number} width of the complete new string incl. number and zeros
 *
-* @returns The given number filled up with leading zeros to a given width (includes the negative sign), returns empty string if number is not an actual number.
+* @returns The given number filled up with leading zeros to a given width (excluding the negative sign), returns empty string if number is not an actual number.
 */
 module.exports.zeroFill = function (number, width) {
     const num = parseInt(number);
