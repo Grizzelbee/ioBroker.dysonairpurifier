7d8a5b9649e806b34a4f07228fe617b7e475db96
Jean Pierre Wenzel <jeanpierre.wenzel@gmail.com>
2020-10-30 22:53:53 +0100

    Made zeroFill work for negative numbers and invalid input

--- dyson-utils.js
+++ dyson-utils.js
@@ -14,8 +14,14 @@ const _ = require('lodash');
 * @param number {number} Value thats needs to be filled up with leading zeros
 * @param width  {number} width of the complete new string incl. number and zeros
 *
-* @returns The given number filled up with leading zeros to a given width
+* @returns The given number filled up with leading zeros to a given width (includes the negative sign), returns empty string if number is not an actual number.
 */
 module.exports.zeroFill = function (number, width) {
-    return `${_.padStart(number, width, '0')}`;
+    const num = parseInt(number);
+
+    if (isNaN(num)) {
+        return '';
+    }
+
+    return (num < 0 ? '-' : '') + `${_.padStart(('' + Math.abs(num)), width, '0')}`;
 };
--- dyson-utils.test.js
+++ dyson-utils.test.js
@@ -18,4 +18,22 @@ describe('dysonUtils => zeroFill', () => {
         });
     });
 
+    it(`should left-pad zeroes for negative input numbers`, () => {
+        const data = [
+            [-5,   4, '-0005'],
+            [-1,   1, '-1'],
+            [-45,  2, '-45'],
+            [-734, 8, '-00000734'],
+        ];
+
+        data.forEach(d => {
+            expect(dysonUtils.zeroFill(d[0], d[1])).to.equal(d[2]);
+        });
+    });
+
+    it(`should return empty string for invalid input`, () => {
+        expect(dysonUtils.zeroFill('', 50)).to.equal('');
+        expect(dysonUtils.zeroFill('ökhdf', 50)).to.equal('');
+    });
+
 });
