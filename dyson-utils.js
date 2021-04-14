9352eecab046a1d1baa03a98765ba1471a9ce4c9
grizzelbee <hanjo@hingsen.de>
2021-04-14 17:57:56 +0200

    fixed jsDoc

--- dyson-utils.js
+++ dyson-utils.js
@@ -21,7 +21,7 @@ rootCas.addFile(path.resolve(__dirname, 'certificates/intermediate.pem'));
  *
  * Formats a number as a string with leading zeros
  *
- * @param number {number} Value thats needs to be filled up with leading zeros
+ * @param number {string} Value thats needs to be filled up with leading zeros
  * @param width  {number} width of the complete new string incl. number and zeros
  *
  * @returns The given number filled up with leading zeros to a given width (excluding the negative sign), returns empty string if number is not an actual number.
